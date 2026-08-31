7828312c652c5017aae430cdd014d13ae4185c8d
aswithamugada <aswithamugada@gmail.com>
2026-08-31 12:01:46 +0530

    msg

---
+++ 06-folder/myapp/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='h-screen w-full bg-gray-600'>
+      <h1 className='dj'>you are my favourite.</h1>
+    </div>
+  )
+}
+
+export default App
